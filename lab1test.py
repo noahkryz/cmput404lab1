1967bce55ba3722b47013ab1f06622664023707f
Noah <nkryzano@ualberta.ca>
2020-01-08 17:59:30 -0700

    raw URL included

--- lab1test.py
+++ lab1test.py
@@ -2,5 +2,8 @@ import requests
 
 print(requests.__version__)
 
-google = requests.get("https://www.google.com")
-print(google)
+# google = requests.get("https://www.google.com")
+# print(google)
+
+var = requests.get("https://raw.githubusercontent.com/noahkryz/cmput404lab1/master/lab1test.py")
+print(var.content)
